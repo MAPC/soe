143f6ef438fa3dec2bc6a825321c25291fd0a0c2
Christian Spanring <cspanring@gmail.com>
2011-03-29 11:10:10 -0400

    minor debug toolbar config change: intercept=false, makes life in admin easier.

--- settings.py
+++ settings.py
@@ -127,6 +127,9 @@ BASE_URL = '/soe/'
 
 # django-debug-toolbar
 INTERNAL_IPS = ('127.0.0.1',)
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
 DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.version.VersionDebugPanel',
     'debug_toolbar.panels.timer.TimerDebugPanel',
