fa6245b8d1c899df4f3b6f7cffebd2827801e8e7
Christian Spanring <cspanring@gmail.com>
2012-08-01 14:25:44 -0400

    serving media in debug mode

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -30,4 +31,12 @@ urlpatterns = patterns('',
     
     
     
+)
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$',
+         'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT, 'show_indexes': True, }),
 )
